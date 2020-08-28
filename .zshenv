e104809fcde8d3a14036e17258e86268a0eb4bb5
Ben Newell <bennewell.mail@gmail.com>
2020-08-27 20:24:36 -0500

    [add]

---
+++ .zshenv
@@ -0,0 +1,8 @@
+# Add `~/bin` to the `$PATH`
+export PATH="$HOME/bin:$PATH";
+
+export PATH="$PATH:/usr/local/bin/"
+
+export PATH="/usr/local/git/bin:/sw/bin/:/usr/local/bin:/usr/local/:/usr/local/sbin:/usr/local/mysql/bin:$PATH"
+
+[ -n "$PS1" ] && source ~/.zshrc;
---
+++ bootstrap.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+echo "Loading";
+cd "$(dirname "${BASH_SOURCE}")";
+echo "";
+
+git pull origin master;
+
+function sync() {
+	rsync --exclude ".git/" \
+		--exclude ".DS_Store" \
+		--exclude ".osx" \
+		--exclude "bootstrap.sh" \
+		--exclude "README.md" \
+		--exclude "LICENSE.txt" \
+		--exclude ".github/" \
+		--exclude ".travis.yml" \
+		--exclude "Brewfile" \
+		--exclude "Rakefile" \
+		--exclude "setup/" \
+		--exclude ".github/" \
+		-avh --no-perms . ~;
+		  if [ -n "$ZSH_VERSION" ]; then
+     source ~/.zshrc;
+  elif [ -n "$BASH_VERSION" ]; then
+     source ~/.bash_profile;
+  else
+     echo 'unknown shell'
+  fi
+}
+
+if [ "$1" == "--force" -o "$1" == "-f" ]; then 
+	sync;
+else
+	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
+	echo "";
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		sync;
+	fi;
+fi;
+
+unset sync;                                                    
+
