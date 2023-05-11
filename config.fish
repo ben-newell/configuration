e138ee30a5a8930f888c0da06f2599f38270a36b
ben-newell <bennewell.mail@gmail.com>
2023-05-10 22:39:19 -0500

    [add] config.fish

--- config.fish
+++ config.fish
@@ -9,7 +9,17 @@ if status is-interactive
     fish_vi_key_bindings
 
     alias g='git'
-    abbr -U g 'git'
+    abbr -a g 'git'
+
+    alias ll 'exa -l --color=always --icons --group-directories-first --header'
+    alias la 'exa -a --color=always --icons --group-directories-first --header'
+    alias lha 'exa -lha --color=always --icons --group-directories-first --header --git'
+    alias v 'exa -alh --color=always --icons --group-directories-first --header --git --extended'
+    function o
+        open $argv
+    end
+
+    complete -c o -a "(command ls)"
 
 end
 
