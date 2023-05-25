7b2190a6d67b40203443f986b59b37cfed4f743c
ben-newell <bennewell.mail@gmail.com>
2023-05-25 15:58:27 -0500

    [add] path executable folders for fish lingo

--- config.fish
+++ config.fish
@@ -10,6 +10,7 @@ if status is-interactive
 
     alias g='git'
     abbr -a g 'git'
+
     alias ls 'lsd --group-dirs first'  # --group-dirs first will list directories first
     alias ll 'lsd -l'  # -l is used to display in long format
     alias la 'lsd -la'  # -a will show all files including hidden files and directories
@@ -23,6 +24,14 @@ if status is-interactive
     alias c='clear'
 
     set -Ua fish_user_paths /opt/homebrew/sbin $fish_user_paths
+
+    # Set PATH
+    set -x PATH $HOME/.rbenv/bin $PATH
+    set -x PATH ~/bin $PATH
+    set -x PATH /Users/morpheus/.cargo/bin $PATH
+    set -x PATH /usr/local/bin $PATH
+    set -x PATH /opt/homebrew/sbin $PATH
+    set -x PATH /opt/homebrew/bin $PATH
 
 end
 
