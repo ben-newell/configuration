1213fb50bb5a8c4bb7b0b32978f04a7011d29232
ben-newell <bennewell.mail@gmail.com>
2023-11-01 12:03:29 -0500

    [remove] random ruby script. Have no idea where it came from

--- ruby.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-cd ~
-rm -rf .rbenv/
-git clone git://github.com/sstephenson/rbenv.git .rbenv
-echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.zshrc
-echo 'eval "$(rbenv init -)"' >> ~/.zshrc
-git clone git://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
-source ~/.zshrc
