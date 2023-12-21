64b98eb0696f63f4e5a02e6427b7ba201221b8dd
ben-newell <bennewell.mail@gmail.com>
2023-12-21 14:18:40 -0600

    [add] ccat

--- brew.sh
+++ brew.sh
@@ -26,6 +26,9 @@ brew install ssh-copy-id
 brew install trippy # ping and traceroute tool, example: trip google.com
 brew install pv # progress bar library
 brew install ripgrep # Search tool like grep and The Silver Searcher
+brew install ccat
+
+
 
 # File Storage
 echo "Installing file storage tools..."
