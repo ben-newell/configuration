40cf8649394f5662f99dac408b02beb2fa2a93f9
Ben Newell <bennewell.mail@gmail.com>
2024-07-29 09:09:29 -0500

    [add] sgrep

--- brew.sh
+++ brew.sh
@@ -81,4 +81,5 @@ brew install pdf-expert
 brew install microsoft-remote-desktop
 brew install font-hack-nerd-font
 brew install vivid # is a generator for the LS_COLORS environment variables
+brew install sgrep
 
